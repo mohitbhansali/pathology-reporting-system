8c2ec5a96a396a77598c5984cd59720acc312889
Mohit Bhansali <mohitbhansali11@gmail.com>
2016-06-16 21:19:13 +0530

    Updated DB name.

--- common/config/main.php
+++ common/config/main.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=crossover',
+            'dsn' => 'mysql:host=localhost;dbname=pathology',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
